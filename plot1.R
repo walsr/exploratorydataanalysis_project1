941d9e0552b1f6bc459e8255ddfe01191aceaf61
Andreas Walser <andreas.walser@swisscom.com>
2015-11-04 21:30:49 +0100

    plot1.R done

--- plot1.R
+++ plot1.R
@@ -1,5 +1,31 @@
 # code to create plot1.png
-#setwd("/Users/tgdwaana/sync/git/exploratorydataanalysis_project1_github/.git/")
+setwd("/Users/tgdwaana/data/")
+
+zipfilename <- "exdata_data_household_power_consumption.zip"
+txtfilename <- "household_power_consumption.txt"
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
-dev.copy(png, file="plot1.png")
+## download and unzip the data
+if (!file.exists(zipfilename)){
+  download.file(url, zipfilename, method="curl")
+}
+
+if (!file.exists(txtfilename)) { 
+  unzip(zipfilename) 
+}
+
+# read data
+data <- read.csv(txtfilename, header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE, dec=".")
+
+# convert date, time variables to date/time classes 
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+#selecting only 2007-02-01 and 2007-02-02 
+data.selection <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+
+#plot
+hist(data.selection$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+## save to file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
