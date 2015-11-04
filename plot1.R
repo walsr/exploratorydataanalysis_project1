5064b93bbbf136c381ee0458bee86b9202d9072d
Andreas Walser <andreas.walser@swisscom.com>
2015-11-04 16:27:22 +0100

    added plot1.R

---
+++ plot1.R
@@ -0,0 +1,5 @@
+# code to create plot1.png
+#setwd("/Users/tgdwaana/sync/git/exploratorydataanalysis_project1_github/.git/")
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+dev.copy(png, file="plot1.png")
